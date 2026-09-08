fe7ddcdd651b8c9e26fe522d87eda8b1bea910af
gnauck68-blip <gnauck68@googlemail.com>
2026-09-08 17:35:21 +0200

    Add files via upload

---
+++ dtf-druckdaten/sw.js
@@ -0,0 +1,31 @@
+var CACHE = 'dtf-druckdaten-v1';
+var FILES = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png'];
+
+self.addEventListener('install', function (e) {
+  e.waitUntil(
+    caches.open(CACHE).then(function (c) { return c.addAll(FILES); })
+      .then(function () { return self.skipWaiting(); })
+  );
+});
+
+self.addEventListener('activate', function (e) {
+  e.waitUntil(
+    caches.keys().then(function (keys) {
+      return Promise.all(keys.map(function (k) { return k === CACHE ? null : caches.delete(k); }));
+    }).then(function () { return self.clients.claim(); })
+  );
+});
+
+self.addEventListener('fetch', function (e) {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    caches.match(e.request).then(function (hit) {
+      if (hit) return hit;
+      return fetch(e.request).then(function (res) {
+        var copy = res.clone();
+        caches.open(CACHE).then(function (c) { try { c.put(e.request, copy); } catch (x) {} });
+        return res;
+      }).catch(function () { return caches.match('./index.html'); });
+    })
+  );
+});
